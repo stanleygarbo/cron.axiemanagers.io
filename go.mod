efc29b0f5836027e6da543767ceb0825053dc5e2
Stanley <stanleygarbo@gmail.com>
2021-09-12 14:56:30 +0800

    added optimizations

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/robfig/cron/v3 v3.0.1 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	gorm.io/driver/postgres v1.1.0 // indirect
 	gorm.io/gorm v1.21.13 // indirect
 )
--- jobs/getDailyEarned.go
+++ jobs/getDailyEarned.go
@@ -1,35 +1,60 @@
 package jobs
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"sync"
 	"time"
 
 	"github.com/stanleygarbo/08_cron.axiemanagers.io/db"
 	"github.com/stanleygarbo/08_cron.axiemanagers.io/entities"
 	"github.com/stanleygarbo/08_cron.axiemanagers.io/util"
+	"golang.org/x/sync/semaphore"
 )
 
 func GetDailyEarned() {
 	roninAddresses := []entities.RoninAddresses{}
-	db.DBConn.Select([]string{"ronin", "last_read"}).Find(&roninAddresses)
+	db.DBConn.Select([]string{"ronin", "last_read"}).Where("last_read >= NOW() - INTERVAL '48 HOURS'").Find(&roninAddresses)
 
-	for _, address := range roninAddresses{
+	wg := &sync.WaitGroup{}
+	sem := semaphore.NewWeighted(100)
+
+	earnings := []entities.Earning{}
+
+	for i, address := range roninAddresses{
 		if address.Ronin != ""{
-			fmt.Printf("@jobs - fetching: %v", address.Ronin)
-			url := fmt.Sprintf("https://game-api.skymavis.com/game-api/clients/%v/items/1", address.Ronin)
-			lunaciaResponse := entities.LunaciaResponse{}
-			err := util.GetResponse(url, &lunaciaResponse)
-			if err != nil{
-				fmt.Printf("@jobs - get daily earned::::Error fetching response:%v\n", err)
-			} else {
-				earning := entities.Earning{
-					Ronin: lunaciaResponse.RoninAddress,
-					Date: int(time.Now().Unix()),
-					Earned: lunaciaResponse.TotalSLP,
-				}
-
-				db.DBConn.Create(&earning)
+			wg.Add(1)
+			if err := sem.Acquire(context.Background(), 1); err != nil{
+				log.Fatalf("Error: %v\n", err)
 			}
+
+			go func(i int,ronin string){
+				// fmt.Printf("@jobs - fetching: %v", address.Ronin)
+				defer wg.Done()
+				defer sem.Release(1)
+				url := fmt.Sprintf("https://game-api.skymavis.com/game-api/clients/%v/items/1", ronin)
+				lunaciaResponse := entities.LunaciaResponse{}
+				err := util.GetResponse(url, &lunaciaResponse)
+				if err != nil{
+					fmt.Printf("@jobs - get daily earned::::Error fetching response:%v\n", err)
+				} else {
+					earning := entities.Earning{
+						Ronin: lunaciaResponse.RoninAddress,
+						Date: int(time.Now().Unix()),
+						Earned: lunaciaResponse.TotalSLP,
+					}
+
+					earnings = append(earnings, earning)
+
+				}
+			}(i, address.Ronin)
 		}
+	}
+
+	wg.Wait()
+
+	if len(earnings) > 0{
+		db.DBConn.Create(&earnings)
 	}
 }
